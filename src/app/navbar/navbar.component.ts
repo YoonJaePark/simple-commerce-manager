bd72173aac8945feff0e37a13b781742e6fba71a
YoonJae Park <rhkdro@gmail.com>
2017-09-12 11:17:42 +0900

    p.334 sidebar category router

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+    appTitle = '상품 관리 시스템';
     constructor() { }
 
     ngOnInit() {
