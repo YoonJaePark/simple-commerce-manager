b3529da6332a9451d2b4d4e8d38c5fd0e9eef14c
YoonJae Park <rhkdro@gmail.com>
2017-09-12 12:41:07 +0900

    p.345 arrange module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
+import { ScmMainModule } from './scm-main/scm-main.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgSwitch } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+// import { SidebarComponent } from './sidebar/sidebar.component';
+// import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    SidebarComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    /* App Modules */
+    ScmMainModule, ProductModule, CategoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/scm-main/scm-main.module.ts
@@ -0,0 +1,20 @@
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const CORE_COMPONENTS = [NavbarComponent, SidebarComponent, FooterComponent, 
+    MainDashboardComponent, PageNotFoundComponent];
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [CORE_COMPONENTS],
+    exports: [CORE_COMPONENTS]
+})
+export class ScmMainModule { }
